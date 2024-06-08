938a62259963f9d7ecf0e4b05a0c289e076c5436
Md Towhidul Alam <alammdtowhidul9@gmail.com>
2024-06-08 18:58:13 +0600

    08.06.2024

--- JDBCSQLConnect/src/jdbcsqlconnect/JDBCSQLConnect.java
+++ JDBCSQLConnect/src/jdbcsqlconnect/JDBCSQLConnect.java
@@ -1,4 +1,3 @@
-
 package jdbcsqlconnect;
 
 import entity.Student;
@@ -10,44 +9,54 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 public class JDBCSQLConnect {
-
+    
     static PreparedStatement ps;
     static ResultSet rs;
-    static DbUtil db=new DbUtil();
-    
+    static DbUtil db = new DbUtil();
     
     public static void main(String[] args) {
+
+//     saveData();
+
+        System.out.println("----------before editing data----------");
+        showData();
+        Student student = new Student();
         
-//    Student student=new Student();
-//    
-//    List<Student>studentList=getById(3);
-//    
-//    student.setName(studentList.get(0).getName());
-//    student.setEmail("wexcrbyunim,ol.;[[");
-//    student.setAddress(studentList.get(0).getAddress());
-//    student.setCell(studentList.get(0).getCell());
-//     student.setId(studentList.get(0).getId());
-//     
-//     
-//        editData(student);
-//        showData();
-     
-     saveData();
-     showData();
-     
-     Student student=new Student(4, "werxsgv", "crfgvy", "4sexcfgh", "+968");
+        List<Student> studentList = getById(3);
+        
+        student.setName(studentList.get(0).getName());
+        student.setEmail("wexcrbyunim,ol.;[[");
+        student.setAddress(studentList.get(0).getAddress());
+        student.setCell(studentList.get(0).getCell());
+        student.setId(studentList.get(0).getId());
+        
+        System.out.println("------------after editing data----------");
         editData(student);
         showData();
-     
+
+//     showData();
+//     
+//     Student student=new Student(4, "werxsgv", "crfgvy", "4sexcfgh", "+968");
+//        editData(student);
+//        showData();
+
+        Student s = new Student();
+        
+        s.setId(5);
+        
+        deleteData(s);
+        
+        System.out.println("---------------before deleting data-------------");
+       showData();
     }
     
-    public static void saveData(){
-    
-    String insertSql="insert into student(name,email,address,cell) values(?,?,?,?)";
+    public static void saveData() {
+        
+        String insertSql = "insert into student(name,email,address,cell) values(?,?,?,?)";
         try {
-            ps=db.getCon().prepareStatement(insertSql);
+            ps = db.getCon().prepareStatement(insertSql);
+            
             ps.setString(1, "Towhid");
             ps.setString(2, "towhid@gmail.com");
             ps.setString(3, "Naogaon");
@@ -60,46 +69,44 @@ public class JDBCSQLConnect {
         } catch (SQLException ex) {
             Logger.getLogger(JDBCSQLConnect.class.getName()).log(Level.SEVERE, null, ex);
         }
-      
-    
-    
-    
+        
     }
     
-    public static void showData(){
-    
-     String selectsql="select * from student";
-     
+    public static void showData() {
+        
+        String selectsql = "select * from student";
+        
         try {
-            ps=db.getCon().prepareStatement(selectsql);
-           rs=ps.executeQuery();
+            ps = db.getCon().prepareStatement(selectsql);
+            rs = ps.executeQuery();
             
-             while(rs.next()){
-                int id=rs.getInt("id");
-                String name=rs.getString("name");
-                String email=rs.getString("email");
-                String address=rs.getString("address");
-                String cell=rs.getString("cell");
+            while (rs.next()) {
                 
-                System.out.println("Id is "+id+" name: "+name +" Email: "+email
-                +" Address: "+address+ " Cell No :"+cell );
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String address = rs.getString("address");
+                String cell = rs.getString("cell");
+                
+                System.out.println("Id is " + id + " name: " + name + " Email: " + email
+                        + " Address: " + address + " Cell No :" + cell);
             }
-             
-             ps.close();
-             rs.close();
-             db.getCon().close();
-             
+            
+            ps.close();
+            rs.close();
+            db.getCon().close();
+            
         } catch (SQLException ex) {
             Logger.getLogger(JDBCSQLConnect.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
-    public static void editData(Student s){
-    
-    String sql="update student set name=?,email=?,address=?,cell=? where id=?";
-    
+    public static void editData(Student s) {
+        
+        String sql = "update student set name=?,email=?,address=?,cell=? where id=?";
+        
         try {
-            ps=db.getCon().prepareStatement(sql);
+            ps = db.getCon().prepareStatement(sql);
             
             ps.setString(1, s.getName());
             ps.setString(2, s.getEmail());
@@ -113,46 +120,59 @@ public class JDBCSQLConnect {
             
             System.out.println("Edit successfully");
             
-            
         } catch (SQLException ex) {
             Logger.getLogger(JDBCSQLConnect.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        
     }
     
-//    public static List<Student>getById(int id){
-//    
-//        List<Student>list=new ArrayList<>();
-//        
-//        String sql="select * from student where id=?";
-//        
-//        try {
-//            ps=db.getCon().prepareStatement(sql);
-//            
-//            ps.setInt(1, id);
-//            
-//            rs=ps.executeQuery();
-//            
-//            while(rs.next()){
-//            
-//                Student s=new Student(
-//                        rs.getInt("id"), 
-//                        rs.getString("name"), 
-//                        rs.getString("email"), 
-//                        rs.getString("address"), 
-//                        rs.getString("cell")
-//                );
-//              
-//            list.add(s);
-//            }
-//            
-//            
-//        } catch (SQLException ex) {
-//            Logger.getLogger(JDBCSQLConnect.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//    return list;
-//    }
+    public static List<Student> getById(int id) {
+        
+        List<Student> list = new ArrayList<>();
+        
+        String sql = "select * from student where id=?";
+        
+        try {
+            ps = db.getCon().prepareStatement(sql);
+            
+            ps.setInt(1, id);
+            
+            rs = ps.executeQuery();
+            
+            while (rs.next()) {
+                
+                Student s = new Student(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("email"),
+                        rs.getString("address"),
+                        rs.getString("cell")
+                );
+                
+                list.add(s);
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(JDBCSQLConnect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
     
+    public static void deleteData(Student s) {
+        
+        String sql = "delete from student where id=?";
+        
+        try {
+            ps = db.getCon().prepareStatement(sql);
+            ps.setInt(1, s.getId());
+            
+            ps.executeUpdate();
+            ps.close();
+            db.getCon().close();
+        } catch (SQLException ex) {
+            Logger.getLogger(JDBCSQLConnect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
     
 }
